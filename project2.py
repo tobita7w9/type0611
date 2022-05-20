b34da99290fbf4d947bae18f6446868d2ce86728
tobita7w9 <105622115+tobita7w9@users.noreply.github.com>
2022-05-20 12:25:03 +0900

    Update project2.py

--- project2.py
+++ project2.py
@@ -2,7 +2,7 @@ def main():
     # 昇順にソートされた配列
     sorted_array = [1, 2, 3, 5, 12, 7890, 12345]
     # 探索対象の番号
-    target_number = 7890
+    target_number = 5
     # 探索実行
     target_index = serch_index(sorted_array, target_number)
     # 結果出力
